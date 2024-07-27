33235b1ac7a3985d86ed797ff66d0b4f148065ae
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 02:50:21 +0700

    21. Else If Statement Makin Banyak Pilihan

---
+++ 21. Else If Statement Makin Banyak Pilihan/app.js
@@ -0,0 +1,34 @@
+const iniHari = "minggu";
+
+if (iniHari === "senin") {
+  console.log("ini senin");
+} else if (iniHari === "selasa") {
+  console.log("ini selasa");
+} else if (iniHari === "rabu") {
+  console.log("ini rabu");
+} else if (iniHari === "kamis") {
+  console.log("ini kamis");
+} else if (iniHari === "jumat") {
+  console.log("ini jumat");
+} else if (iniHari === "sabtu") {
+  console.log("ini sabtu");
+} else if (iniHari === "minggu") {
+  console.log("ini minggu");
+}
+
+
+const nilai = 10;
+
+switch (nilai) {
+  case 10:
+    console.log("ini 10");
+    break;
+  case 20:
+    console.log("ini 20");
+    break;
+  case 30:
+    console.log("ini 30");
+    break;
+  default:
+    console.log("ini default");
+}
