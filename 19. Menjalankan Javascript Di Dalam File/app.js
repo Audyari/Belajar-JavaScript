78107e323e273cd95399a7c1d86c7ee1c7c924ea
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 02:12:47 +0700

    19. Menjalankan Javascript Di Dalam File

---
+++ 19. Menjalankan Javascript Di Dalam File/app.js
@@ -0,0 +1,6 @@
+// Create an event listener for the button click
+function executeJavascript() {
+  document.getElementById("output").innerHTML = 1 + 1;
+  alert("Hello World");
+  console.log("Hello World");
+}
