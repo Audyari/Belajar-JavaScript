57d83abc13190190213854f07b0759abc702b3b3
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 03:04:32 +0700

    22. Else Adalah Pilihan Terakhir

---
+++ 22. Else Adalah Pilihan Terakhir/app.js
@@ -0,0 +1,10 @@
+const iniHari = prompt("Hari Favorite Saya").toLowerCase();
+
+
+if (iniHari === "senin") {
+  console.log("ini senin");
+} else if (iniHari === "selasa") {
+  console.log("ini selasa");
+} else {
+  console.log("Bukan Hari Favorite Saya");
+}
