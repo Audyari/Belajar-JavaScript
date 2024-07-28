bd7cf3b9bbd0364c68d0d3a8bc1acfb1a5239056
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 10:41:52 +0700

    28. Mengenal Struktur Data Array

---
+++ 28. Mengenal Struktur Data Array/app.js
@@ -0,0 +1,30 @@
+//28. Mengenal Struktur Data Array
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array);
+
+let array2 = ["Audyari", "Wiyono", "Jaya", "Kencana"];
+console.log(array2);
+
+let array3 = [
+  "Audyari",
+  "Wiyono",
+  "Jaya",
+  "Kencana",
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+];
+console.log(array3);
+
+let arrayKosong = [];
+console.log(arrayKosong);
+
+console.log(typeof arrayKosong);
