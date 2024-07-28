d22c1dadcb16bf05010f858e2fcd18a979647672
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 10:10:52 +0700

    26. Operator Logic Not

---
+++ 26. Operator Logic Not/app.js
@@ -0,0 +1,10 @@
+console.log("!true : ", !true);
+console.log("!false : ", !false);
+
+const role = prompt("Masukan Role Anda : ");
+
+if (role !== "admin") {
+  console.log("anda bukan admin");
+} else {
+  console.log("anda admin");
+}
