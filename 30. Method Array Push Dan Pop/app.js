dd0866b695ee4440004aeadfa0ff47fc6a13d653
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 12:00:12 +0700

    30. Method Array Push Dan Pop

---
+++ 30. Method Array Push Dan Pop/app.js
@@ -0,0 +1,21 @@
+// Method Array Push Dan Pop
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array);
+
+array.push(11);
+console.log(array);
+
+array.pop();
+console.log(array);
+
+array.pop();
+console.log(array);
+
+console.log(array.length);
+
+array.shift();
+console.log(array);
+
+array.unshift("data");
+console.log(array);
