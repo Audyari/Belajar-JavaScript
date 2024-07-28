b6e2d77ba4d5f4a23d15354a56ab3ac8594d2b5a
Audyari Wiyono <audy123ari@gmail.com>
2024-07-28 11:39:16 +0700

    29. Mendapatkan Nilai Dengan Index Dan Mengubah Isinya

---
+++ 29. Mendapatkan Nilai Dengan Index Dan Mengubah Isinya/app.js
@@ -0,0 +1,13 @@
+//Mendapatkan Nilai Dengan Index Dan Mengubah Isinya
+
+let array = [2, 1, 3, 4, 5, 6, 7, 8, 10, 9];
+console.log(array);
+
+array[0] = "Audyari";
+// console.log(array);
+
+array[1] = "Wiyono";
+// console.log(array);
+
+array[12] = "Jaya";
+console.log(array);
