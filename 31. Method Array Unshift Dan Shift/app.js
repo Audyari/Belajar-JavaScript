edfb4f8a5bfc99a2703d18629dbc2edcd6f373e5
Audyari Wiyono <audy123ari@gmail.com>
2024-07-29 08:47:54 +0700

    31. Method Array Unshift Dan Shift

---
+++ 31. Method Array Unshift Dan Shift/app.js
@@ -0,0 +1,10 @@
+
+let Murid = ["Audyari", "Wiyono", "Jaya", "Kencana"];
+console.log(Murid);
+
+Murid.unshift("Audi"); //Method Unshift Digunakan Untuk Menambahkan Data Pada Awal Array
+console.log(Murid);
+
+Murid.shift(); // Method Shift Digunakan Untuk Menghapus Data Pada Awal Array
+console.log(Murid)
+
