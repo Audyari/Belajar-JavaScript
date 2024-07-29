eee57b86b183c1f69eb414c0f7c227edf676d5f5
Audyari Wiyono <audy123ari@gmail.com>
2024-07-29 10:17:17 +0700

    35. Array Multidimensi Atau Nested Array

---
+++ 35. Array Multidimensi Atau Nested Array/app.js
@@ -0,0 +1,17 @@
+
+
+// Array Multidimensi Atau Nested Array
+
+let array = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+];
+
+console.log(array);
+
+console.log(" array[0][2] ",array[0][2]);
+
+console.log(" array[1][3] ",array[1][3]);
+
+console.log(" array[2][4] ",array[2][4]);
