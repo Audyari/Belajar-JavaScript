c420aabb5b27e2c6291038594f6aba8ca88afd98
Audyari Wiyono <audy123ari@gmail.com>
2024-07-29 10:46:41 +0700

    37. Cara Memanggil Data Dari Object

---
+++ 37. Cara Memanggil Data Dari Object/app.js
@@ -0,0 +1,22 @@
+
+// Cara Memanggil Data Dari Object
+
+let mahasiswa = {
+    nama : "Audyari Wiyono",
+    umur : 41,
+    email : "6Yn8t@example.com",
+    jurusan : "Teknik Informatika",
+    alamat : {
+        kota : "Pondok Gede",
+        provinsi : "Jawa Tengah"
+    }
+}
+
+console.log(mahasiswa.umur)
+
+console.log(mahasiswa.alamat.kota)
+
+console.log(mahasiswa.alamat.provinsi)
+
+
+
