8eacebee9e9cc4d936f1382268a1b8caedec7a2d
Audyari Wiyono <audy123ari@gmail.com>
2024-07-29 10:31:43 +0700

    36. Cara Membuat Struktur Data Object

---
+++ 36. Cara Membuat Struktur Data Object/app.js
@@ -0,0 +1,16 @@
+
+
+// Cara Membuat Struktur Data Object
+
+let mahasiswa = {
+    nama : "Audyari Wiyono",
+    umur : 41,
+    email : "6Yn8t@example.com",
+    jurusan : "Teknik Informatika",
+    alamat : {
+        kota : "Pondok Gede",
+        provinsi : "Jawa Tengah"
+    }
+}
+
+console.log(mahasiswa)
