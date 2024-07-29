c020fbcd998f34db2db583fae2fc59a4306b5b75
Audyari Wiyono <audy123ari@gmail.com>
2024-07-29 09:52:57 +0700

    33. Beberapa Method Array Yang Sering Digunakan Lagi

---
+++ 33. Beberapa Method Array Yang Sering Digunakan Lagi/app.js
@@ -0,0 +1,30 @@
+
+// Penggunaan untuk Array Splice 
+
+let array = ["Audyari", "Wiyono", "Jaya", "Kencana"];
+array.splice(2, 0, "Kencana", "Jaya");
+console.log("Penggunaan Array Splice",array);
+
+// Penggunaan untuk Array Slice
+
+let array2 = ["Audyari", "Wiyono", "Jaya", "Kencana"];
+let array3 = array2.slice(1, 3);
+console.log("Penggunaan Array Slice",array3);
+
+// Penggunaan untuk Array Reverse
+
+let array4 = ["Audyari", "Wiyono", "Jaya", "Kencana"];
+array4.reverse();
+console.log("Penggunaan Array Reverse",array4);
+
+// Penggunaan untuk Array Join
+
+let array5 = ["Audyari", "Wiyono", "Jaya", "Kencana"];
+
+console.log("Penggunaan Array Join",array5.join(" - "));
+
+
+
+
+
+
