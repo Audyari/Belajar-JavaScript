196a68c8d8be580eb7f72c1d4ec2e44a78c7e87b
Audyari Wiyono <audy123ari@gmail.com>
2024-07-29 09:19:41 +0700

    32. Beberapa Method Array Yang Sering Digunakan

---
+++ 32. Beberapa Method Array Yang Sering Digunakan/app.js
@@ -0,0 +1,23 @@
+
+// Penggunaan Array Concat
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let array2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+let array3 = array.concat(array2);
+console.log("Penggunaan Array Concat ",array3)
+
+// Penggunaan Array indexOf
+
+let array4 = ["Audyari", "Wiyono", "Jaya", "Kencana"];
+console.log("Penggunaan Array indexOf ",array4.indexOf("Jaya"))
+
+//  Penggunaan Array includes
+
+console.log("Penggunaan Array includes ",array4.includes("Wiyono"))
+
+// Penggunaan Array sort 
+
+console.log("Penggunaan Array sort ",array3.sort()) 
+
+
