13081e3dd31fd798f325a604ce547f6737eed26a
Audyari Wiyono <audy123ari@gmail.com>
2024-07-30 09:05:28 +0700

    41. Contoh Lain Perulangan For

---
+++ 41. Contoh Lain Perulangan For/app.js
@@ -0,0 +1,18 @@
+// Contoh Lain Perulangan For nilai genap
+
+for (let i = 0; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+// Contoh Lain Perulangan For nilai ganjil
+
+for (let i = 0; i <= 10; i++) {
+    if (i % 2 == 1) {
+        console.log(i);
+    }
+}
+
+
+ 
